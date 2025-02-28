e969dd9141050d40788a00707138b112eeabdd7c
Brendon Butler <brendonaaron97@gmail.com>
2025-02-27 21:17:20 -0500

    GH-30 update start.sh for spigot and sponge servers

--- servers/spigot-server/start.sh
+++ servers/spigot-server/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd "$( dirname "$0" )" || exit
-java -Xms500M -Xmx1200M -XX:+UseG1GC -jar spigot-*.jar nogui
+/opt/homebrew/Cellar/openjdk@17/17.0.13/bin/java -Xms500M -Xmx1200M -XX:+UseG1GC -jar spigot-*.jar nogui
